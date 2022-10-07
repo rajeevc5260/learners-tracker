f0a69e36dbc1329d66b54dbfc90513e6e413fc95
Rajeev C <81346240+rajeevc5260@users.noreply.github.com>
2022-10-07 21:22:00 +0530

    adminSettings changed

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './Components/login/login.component';
 import { DashboardSidenavComponent } from './Components/dashboard-sidenav/dashboard-sidenav.component';
 import { AdminSettingsComponent } from './Components/admin-settings/admin-settings.component';
 import { PlacementsComponent } from './Components/placements/placements.component';
+import { DashboardContentComponent } from './Components/dashboard-content/dashboard-content.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardSidenavComponent,
     children: [
+      { path: 'dashboardContent', component: DashboardContentComponent},
       { path: 'learnersAnalytics', component: LearnersAnalyticsComponent},
       { path: 'adminSettings', component: AdminSettingsComponent },
       { path: 'placements', component: PlacementsComponent },
